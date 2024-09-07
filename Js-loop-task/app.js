f8c5e922e56fcda6e784ec58a318b0a0799377c0
jaman hasan <jamanhasan246@gmail.com>
2024-09-07 17:37:21 +0600

    i have done my loop problem number 5

--- Js-loop-task/app.js
+++ Js-loop-task/app.js
@@ -75,3 +75,20 @@ Write a loop to print even numbers from 1 to 40. Use continue to skip odd number
 //     }
     
 // }
+
+
+/*
+display odd number from 55 to 85 and skip the numbers divisible by 5.
+*/
+// problem 5 ans=============>>>>>>>>>>>>>>
+
+
+// for (let i = 55; i < 85; i++) {
+//    if (i%2==1 && i%5 !== 0 ) {
+//      console.log(i);
+     
+//    }
+    
+// }
+
+
